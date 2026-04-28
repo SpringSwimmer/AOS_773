96fbcf80d161491d159cc518cc306167b34a946d
Leo Seen <lseen@icecube.wisc.edu>
2026-04-28 11:44:43 -0500

    remove unnecessary files in git commit

--- my_environment/lib/python3.10/site-packages/jacobi/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Fast numerical derivatives for analytic functions with arbitrary round-off error."""
-
-from ._propagate import propagate
-from ._jacobi import jacobi
-from ._version import version as __version__  # noqa
-
-__all__ = ["jacobi", "propagate"]
